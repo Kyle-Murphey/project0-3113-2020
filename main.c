52c7e624df48b29e946268dc74de649d3159dd73
Kyle-Murphey <kyle_murphey@live.com>
2020-02-25 15:25:00 -0600

    reads in bytes and prints

--- main.c
+++ main.c
@@ -6,23 +6,39 @@
 /* unsigned char */
 typedef unsigned char byte;
 
+typedef struct _uni_char
+{
+    int count;
+    const byte * uni_char;
+} _uni_char;
 
 int main(int argc, char** argv) {
     int input;
     byte buffer[128];
+    _uni_char charArray[128];
     int i = 0;
+    int arraySize = 0;
 
     //main input loop, continues until EOF
     while ((input = fgetc(stdin)) != EOF)
     {
         buffer[i] = (byte)input;
+
+        charArray[i].uni_char = (byte*)input;
+        ++charArray[i].count;
+        ++arraySize;
+
         if ((input >= 'A' && input <= 'Z') || (input >= 'a' && input <= 'z') || (input >= 0xC0))
         {
-            printf("Char->%c\n", buffer[i]);
+            printf("Char->%c\n", buffer[i++]);
         }
 
     }
 
     printf("%d\n", input);
+    for (int x = 0; x < arraySize; ++x)
+    {
+        printf("Count->%d\nChar->%c\n", charArray[x].count, *charArray[x].uni_char);
+    }
     return 0;
 }
