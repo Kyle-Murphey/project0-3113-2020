2f080936721e1946c027ab3b95d661f222280ad4
Kyle-Murphey <kyle_murphey@live.com>
2020-02-26 10:37:09 -0600

    reads in chars and stores into array of structs.

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
 
 /* unsigned char */
 typedef unsigned char byte;
@@ -9,36 +11,70 @@ typedef unsigned char byte;
 typedef struct _uni_char
 {
     int count;
-    const byte * uni_char;
+    byte * uni_char;
 } _uni_char;
 
 int main(int argc, char** argv) {
     int input;
-    byte buffer[128];
+    byte inputByte;
     _uni_char charArray[128];
-    int i = 0;
     int arraySize = 0;
+    int flag = FALSE;
+
 
     //main input loop, continues until EOF
-    while ((input = fgetc(stdin)) != EOF)
+    while (((input = fgetc(stdin)) != EOF))
     {
-        buffer[i] = (byte)input;
+        //exit char
+        if (input == '$')
+        {
+            break;
+        }
 
-        charArray[i].uni_char = (byte*)input;
-        ++charArray[i].count;
-        ++arraySize;
+        inputByte = (byte)input;
+
+        if (arraySize == 0)
+        {
+            //charArray[0].uni_char = &buffer[i];
+            charArray[0].uni_char = malloc(sizeof(byte));
+            *charArray[0].uni_char = inputByte;
+            charArray[0].count = 1;
+            ++arraySize;
+        } else {
+            for(int i = 0; i < arraySize; ++i)
+            {
+                if (inputByte == *charArray[i].uni_char)
+                {
+                    ++charArray[i].count;
+                    flag = TRUE;
+                    break;
+                }
+            }
+            if (flag != TRUE)
+            {
+                charArray[arraySize].uni_char = malloc(sizeof(byte));
+                *charArray[arraySize].uni_char = inputByte;
+                charArray[arraySize].count = 1;
+                ++arraySize;
+            } else {
+                flag = FALSE;
+            }
+        }
 
         if ((input >= 'A' && input <= 'Z') || (input >= 'a' && input <= 'z') || (input >= 0xC0))
         {
-            printf("Char->%c\n", buffer[i++]);
+            //printf("Char->%c\n", buffer[i++]);
         }
-
     }
-
-    printf("%d\n", input);
-    for (int x = 0; x < arraySize; ++x)
+    // prints the contents of the charArray except the newline char
+    for (int i = 0; i < arraySize; ++i)
     {
-        printf("Count->%d\nChar->%c\n", charArray[x].count, *charArray[x].uni_char);
+        if (*charArray[i].uni_char == '\n')
+        {
+            continue;
+        } else{
+            printf("Count->%d : Char->%c\n", charArray[i].count, *charArray[i].uni_char);
+        }
     }
     return 0;
 }
