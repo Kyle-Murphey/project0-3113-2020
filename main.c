300011efc77813d6a1d20dae5c020d3c8c0f4356
Kyle-Murphey <kyle_murphey@live.com>
2020-02-26 15:32:54 -0600

    added ability to determine uni byte length

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <wchar.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -23,6 +25,7 @@ int main(int argc, char** argv) {
     int flag = FALSE;
 
 
+
     FILE * file = fopen("test_uni.txt", "r");
     //file = fopen("test", "r");
 
@@ -37,34 +40,72 @@ int main(int argc, char** argv) {
 
         inputByte = (byte)input;
 
-        if (arraySize == 0)
+        //1111 0xxx (4 bytes of unicode)
+        if (input >= 0xF0 && input <= 0xF7)
         {
-            //charArray[0].uni_char = &buffer[i];
-            charArray[0].uni_char = malloc(sizeof(byte));
-            *charArray[0].uni_char = inputByte;
-            charArray[0].count = 1;
-            ++arraySize;
-        } else {
-            for(int i = 0; i < arraySize; ++i)
+            wchar_t inChar = 0;
+            inChar = inChar << 8 | input;
+            for (int i = 0; i < 3; ++i)
             {
-                if (inputByte == *charArray[i].uni_char)
-                {
-                    ++charArray[i].count;
-                    flag = TRUE;
-                    break;
-                }
+                input = fgetc(file);
+                inChar = inChar << 8 | input;
             }
-            if (flag != TRUE)
+
+            wprintf(L"Out: %lc", inChar);
+        }
+        //1110 xxxx (3 bytes of unicode)
+        else if (input >= 0xE0 && input <= 0xEF)
+        {
+            wchar_t inChar = 0;
+            inChar = inChar << 8 | input;
+            for (int i = 0; i < 2; ++i)
             {
-                charArray[arraySize].uni_char = malloc(sizeof(byte));
-                *charArray[arraySize].uni_char = inputByte;
-                charArray[arraySize].count = 1;
+                input = fgetc(file);
+                inChar = inChar << 8 | input;
+            }
+            wprintf(L"Out:%lc", inChar);
+        }
+        // 110x xxxx (2 bytes of unicode)
+        else if (input >= 0xC0 && input <= 0xDF)
+        {
+            wchar_t inChar = 0;
+
+            inChar = inChar << 8 | input;
+            input = fgetc(file);
+            inChar = inChar << 8 | input;
+
+            vfwprintf(L"Out:%lc", inChar);
+        }
+        //other bytes
+        else {
+            if (arraySize == 0)
+            {
+                //charArray[0].uni_char = &buffer[i];
+                charArray[0].uni_char = malloc(sizeof(byte));
+                *charArray[0].uni_char = inputByte;
+                charArray[0].count = 1;
                 ++arraySize;
             } else {
-                flag = FALSE;
+                for(int i = 0; i < arraySize; ++i)
+                {
+                    if (inputByte == *charArray[i].uni_char)
+                    {
+                        ++charArray[i].count;
+                        flag = TRUE;
+                        break;
+                    }
+                }
+                if (flag != TRUE)
+                {
+                    charArray[arraySize].uni_char = malloc(sizeof(byte));
+                    *charArray[arraySize].uni_char = inputByte;
+                    charArray[arraySize].count = 1;
+                    ++arraySize;
+                } else {
+                    flag = FALSE;
+                }
             }
         }
-
         //if ((input >= 'A' && input <= 'Z') || (input >= 'a' && input <= 'z') || (input >= 0xC0))
         //{
 
