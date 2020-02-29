e909d55ff88a5a7a0932e7d07f5bc9c5866d58ab
Kyle-Murphey <kyle_murphey@live.com>
2020-02-28 20:56:52 -0600

    added comments and README/COLLABORATORS

---
+++ README.md
@@ -0,0 +1,17 @@
+Kyle Murphey, Project 0
+
+Compile: gcc -c main.c
+
+Run: cat file | ./a.out
+
+Bugs: Any character that is outside the range of values would not get displayed.
+
+I assume all characters are in either unicode or ascii.
+
+The main function loops through the file, stores them, and then siplays the characters in descending order.
+The storeChar funtion stores the characters based on whether or not they are unicode.
+The compare function compares two _uni_chars and returns a value for qsort.
+
+Sources:
+	https://www.cprogramming.com/tutorial/unicode.html
+	Linux man pages
--- main.c
+++ main.c
@@ -17,6 +17,12 @@ typedef struct _uni_char
     int isUni; // flag whether or not character is unicode
 } _uni_char;
 
+/*
+ * compare function for qsort
+ * @param:left = left comparator
+ * @param:right = right comparator
+ * @return = value in which qsort will use to sort
+ */
 int compare(const void *left, const void *right)
 {
     const _uni_char *a = (const _uni_char *) left;
@@ -30,25 +36,35 @@ int compare(const void *left, const void *right)
         return &right - &left;
 }
 
-void storeChar(_uni_char *charArray, int *uniqueChars, int *input, FILE *file, int uniSize)
+/*
+ * determines if the character is unicode or ascii, and how long the unicode character is. It then stores the values into charArray
+ * @param:charArray = array for storing characters
+ * @param:uniqueChars = amount of unique characters in the charArray
+ * @param:input = input bit from fgetc
+ * @param:file = file read from
+ * @param:uniSize = size of the charater in bytes
+ */
+void storeChar(_uni_char *charArray, int *uniqueChars, int *input, /*FILE *file,*/ int uniSize)
 {
-    int isDuplicate = FALSE;
-    byte inputChar[5] = {0};
-    inputChar[0] = (byte)*input;
+    int isDuplicate = FALSE; // flag for duplication
+    byte inputChar[5] = {0}; // array to hold a full length unicode char
+    inputChar[0] = (byte)*input; // storing first byte
 
+    //stores each byte into the array
     for (int i = 1; i < uniSize; ++i)
     {
-        *input = fgetc(file);
+        *input = fgetc(stdin);
         inputChar[i] = (byte)*input;
     }
 
+    //no characters stored yet
     if (*uniqueChars == 0)
     {
         memset(charArray[0].uni_char, 0, sizeof(charArray[0].uni_char));
-        strncpy(charArray[0].uni_char, inputChar, uniSize);
+        strncpy(charArray[0].uni_char, inputChar, uniSize); //store the contents of the inputChar into the charArray
         charArray[0].count = 1;
-        charArray[0].isUni = TRUE;
 
+        //unicode character
         if (uniSize > 1)
             charArray[0].isUni = TRUE;
         else
@@ -58,8 +74,10 @@ void storeChar(_uni_char *charArray, int *uniqueChars, int *input, FILE *file, i
     }
     else
     {
+        //loop to compare character to find duplicates
         for (int i = 0; i < *uniqueChars; ++i)
         {
+            //unicode character
             if (charArray[i].isUni == TRUE)
             {
                 if (strcmp(inputChar, charArray[i].uni_char) == 0)
@@ -69,6 +87,7 @@ void storeChar(_uni_char *charArray, int *uniqueChars, int *input, FILE *file, i
                     break;
                 }
             }
+            //ascii
             else
             {
                 if ((byte)*input == *charArray[i].uni_char)
@@ -79,6 +98,7 @@ void storeChar(_uni_char *charArray, int *uniqueChars, int *input, FILE *file, i
                 }
             }
         }
+        // new character
         if (isDuplicate != TRUE)
         {
             memset(charArray[*uniqueChars].uni_char, 0, sizeof(charArray[0].uni_char));
@@ -95,19 +115,22 @@ void storeChar(_uni_char *charArray, int *uniqueChars, int *input, FILE *file, i
     }
 }
 
-
+/*
+ * reads in a file of unicode/ascii and displays the frequency of the characters in descending order
+ */
 int main(int argc, char** argv) {
     int input; // var used for reading and storing byte
-    int arraySize = 23; // size of the array
+    int arraySize = 50; // base size of the array
     _uni_char *charArray; // array of characters
     int uniqueChars = 0; // amount of unique characters
-    FILE * file = fopen("test_uni.txt", "r");
+    //FILE * file = fopen("test_uni.txt", "r"); // file for testing
 
-    charArray = malloc(arraySize * sizeof(_uni_char));
+    charArray = malloc(arraySize * sizeof(_uni_char)); // allocating room for the characters
 
     //main input loop, continues until EOF
-    while (((input = fgetc(file)) != EOF))
+    while (((input = fgetc(stdin)) != EOF))
     {
+        //increases the size of the mem allocated by 50% if it gets close to filling up
         if (arraySize - uniqueChars == 1)
         {
             arraySize += (int)(arraySize*0.5);
@@ -116,31 +139,32 @@ int main(int argc, char** argv) {
         //1111 0xxx (4 bytes of unicode)
         if (input >= 0xF0 && input <= 0xF7)
         {
-            storeChar(charArray, &uniqueChars, &input, file, 4);
+            storeChar(charArray, &uniqueChars, &input, /*file,*/ 4);
         }
         //1110 xxxx (3 bytes of unicode)
         else if (input >= 0xE0 && input <= 0xEF)
         {
-            storeChar(charArray, &uniqueChars, &input, file, 3);
+            storeChar(charArray, &uniqueChars, &input, /*file,*/ 3);
         }
         // 110x xxxx (2 bytes of unicode)
         else if (input >= 0xC0 && input <= 0xDF)
         {
-            storeChar(charArray, &uniqueChars, &input, file, 2);
+            storeChar(charArray, &uniqueChars, &input, /*file,*/ 2);
         }
         //other bytes
         else
         {
-            storeChar(charArray, &uniqueChars, &input, file, 1);
+            storeChar(charArray, &uniqueChars, &input, /*file,*/ 1);
         }
     }
-    fclose(file);
+    //fclose(file); // close the file
 
-    qsort(charArray, uniqueChars, sizeof(_uni_char), compare);
+    qsort(charArray, uniqueChars, sizeof(_uni_char), compare); // sort the characters
 
-    // prints the contents of the charArray except the newline char
+    //prints the contents of the charArray
     for (int i = 0; i < uniqueChars; ++i)
     {
+        //not unicode
         if (charArray[i].isUni == FALSE)
         {
             if (*charArray[i].uni_char == '\n' || *charArray[i].uni_char == '\r')
@@ -152,6 +176,7 @@ int main(int argc, char** argv) {
                 printf("%c->%d\n", *charArray[i].uni_char, charArray[i].count);
             }
         }
+        //unicode
         else
         {
             if (*charArray[i].uni_char == '\n' || *charArray[i].uni_char == '\r')
